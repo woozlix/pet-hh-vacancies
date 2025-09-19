25c3cd63aaa1d18202acdf9d3639c600311ad41a
woozlic <sampaccs@mail.ru>
2025-09-19 14:02:17 +0300

    add logging, fix json file encoding

--- vacancies.py
+++ vacancies.py
@@ -1,7 +1,12 @@
 import json
 import enum
+import logging
 
 import requests
+
+logging.basicConfig(
+    level=logging.INFO,
+)
 
 
 class Experience(enum.Enum):
@@ -15,7 +20,7 @@ class ApiHH:
     def __init__(self):
         self.uri = "https://api.hh.ru/"
         self.headers = {
-            "User-Agent": "hh-parser/2.0 (sampaccs@mail.ru)",
+            "User-Agent": "hh-parser/2.0 (test@mail.ru)",
         }
 
     @property
@@ -59,15 +64,16 @@ class ApiHH:
             if row['name'] == name:
                 return row['id']
         else:
-            print(f'Experience not found in list: {str(experience_list)}')
+            raise ValueError(f'Experience not found in list: {str(experience_list)}')
 
     def get_vacancies(self, query_filter: dict) -> dict:
         result = requests.get(self.uri_vacancies, params=query_filter, headers=self.headers)
         if result.status_code == 200:
+            logging.debug(f'Request to {self.uri_vacancies} OK')
             result_json = result.json()
             return result_json
         else:
-            print(f'Error getting vacancies: {result.status_code = }, {result.text}')
+            logging.error(f'Error getting vacancies: {result.status_code = }, {result.text}')
             return {}
 
     def find_area_id(self, city: str) -> str:
@@ -79,6 +85,7 @@ class ApiHH:
             raise ValueError(f'No city with name {city}')
 
     def parse_vacancies(self):
+        logging.info('Started parsing hh.ru')
         filter_city = 'Москва'
         filter_experience = 'От 3 до 6 лет'
         query_filter = {
@@ -88,19 +95,26 @@ class ApiHH:
             'per_page': 100,
             'page': 0
         }
+        logging.info(
+            f'Set filters:'
+            f'{query_filter}'
+        )
         result = []
         while True:
             vacancies_page_result = self.get_vacancies(query_filter)
             pages = vacancies_page_result['pages']
             if query_filter['page'] == pages - 1:
+                logging.info(f'Got all pages of vacancies.')
                 return result
             else:
                 query_filter['page'] += 1
                 result += vacancies_page_result['items']
 
-    def write_vacancies(self, vacancies_list: list[dict], filename: str):
-        with open(filename, 'w') as f:
-            f.write(json.dumps(vacancies_list))
+    @staticmethod
+    def write_vacancies(vacancies_list: list[dict], filename: str):
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write(json.dumps(vacancies_list, ensure_ascii=False, indent=2))
+            logging.info(f'All vacancies saved as {filename}')
 
 
 if __name__ == '__main__':
